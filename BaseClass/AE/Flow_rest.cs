b279936867f215462f2bcaf0f5c3c5b598b31cf4
ben14784 <ben14784@gmail.com>
2024-08-27 13:56:45 +0800

    TEST

--- BaseClass/AE/Flow_rest.cs
+++ BaseClass/AE/Flow_rest.cs
@@ -1,9 +1,10 @@
 ﻿namespace KF_WebAPI.BaseClass.AE
 {
-    public class Flow_rest
+    public class Flow_rest 
     {
         public string? FR_id { get; set; }
         public string? FR_cknum { get; set; }
+        public string? FR_cknum_1 { get; set; }
 
         public tbInfo? tbInfo { get; set; }
     }
