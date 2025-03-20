f0a054163c48bd6b3ccfff51672406a0a68ed6e8
ben14784 <ben14784@gmail.com>
2025-03-20 16:28:48 +0800

    ben_TEST

--- Program.cs
+++ Program.cs
@@ -62,7 +62,8 @@ app.UseCors(builder =>
         "https://www.kuofongweb.com.tw/",
         "http://localhost:7135",
         "http://localhost:8080",
-        "http://192.168.1.240:8080",       
+        "http://192.168.1.240:8080",
+         "http://192.168.1.240:8088",
         "http://192.168.1.240:8081"
         )
            .AllowAnyMethod()
