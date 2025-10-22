782a850b0e701d41c31e5e73392e859f95ae095d
weskey32602 <weskey32602@gmail.com>
2025-10-22 17:43:00 +0800

    財會單位判定 ChkUserRole

--- Controllers/AE_VPController.cs
+++ Controllers/AE_VPController.cs
@@ -566,7 +566,7 @@ namespace KF_WebAPI.Controllers
             {
                 ADOData _adoData = new ADOData();
                 #region SQL
-                var T_SQL = @"select * from User_M where U_num=@User and Role_num in ('1004','1005','1020','1006','1007')";
+                var T_SQL = @"select * from User_M where U_num=@User and Role_num in ('1004','1005','1020')";
                 var parameters = new List<SqlParameter> 
                 {
                     new SqlParameter("@User", User)
