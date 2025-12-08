3260d7d945c9662d5aa399cfe06278a8e9aa06fb
weskey32602 <weskey32602@gmail.com>
2025-12-08 15:36:02 +0800

    客戶來電分析表(網路)

---
+++ BaseClass/AE/Report.cs
@@ -0,0 +1,10 @@
+﻿namespace KF_WebAPI.BaseClass.AE
+{
+    public class Incoming_req
+    {
+        public string? TelAsk { get; set; }
+        public string? TelSour { get; set; }
+        public string? Fin_type { get; set; }
+    }
+
+}
